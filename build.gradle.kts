436abbc60f17ee3f880cadb8d2be3214208d3b83
elyahou <elyahou.hadass@gmail.com>
2024-10-10 00:48:57 +0300

    windows native

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,9 @@ repositories {
 }
 
 dependencies {
-    implementation("net.java.dev.jna:jna:5.13.0")
+    implementation("net.java.dev.jna:jna:5.15.0")
+    implementation("net.java.dev.jna:jna-platform:5.15.0")
+
     implementation("io.github.kdroidfilter:kmplog:0.1.0")
     testImplementation(kotlin("test"))
 }
