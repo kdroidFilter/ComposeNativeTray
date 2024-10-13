49d7a70efe87554446ed3fa6f82a368495157bd7
Elyahou <elyahou.hadass@gmail.com>
2024-10-13 12:11:18 +0300

    Windows bug correction

--- src/main/kotlin/com/kdroid/composetray/menu/impl/WindowsTrayMenuBuilderImpl.kt
+++ src/main/kotlin/com/kdroid/composetray/menu/impl/WindowsTrayMenuBuilderImpl.kt
@@ -1,9 +1,9 @@
 package com.kdroid.composetray.menu.impl
 
 import com.kdroid.composetray.lib.windows.WindowsTrayManager
-import com.kdroid.composetray.menu.TrayMenu
+import com.kdroid.composetray.menu.api.TrayMenuBuilder
 
-internal class WindowsTrayMenuImpl(val iconPath : String, val tooltip : String = "") : TrayMenu {
+internal class WindowsTrayMenuBuilderImpl(val iconPath : String, val tooltip : String = "") : TrayMenuBuilder {
     private val menuItems = mutableListOf<WindowsTrayManager.MenuItem>()
 
     override fun Item(label: String, isEnabled: Boolean, onClick: () -> Unit) {
@@ -17,22 +17,32 @@ internal class WindowsTrayMenuImpl(val iconPath : String, val tooltip : String =
     }
 
     override fun CheckableItem(label: String, isEnabled: Boolean, onToggle: (Boolean) -> Unit) {
+        var isChecked = false // Initialise l'état checked
+
         menuItems.add(
             WindowsTrayManager.MenuItem(
                 text = label,
                 isEnabled = isEnabled,
                 isCheckable = true,
+                isChecked = isChecked,
                 onClick = {
-                    val currentState = menuItems.find { it.text == label }?.isChecked ?: false
-                    onToggle(!currentState)
+                    // Inverse l'état checked
+                    isChecked = !isChecked
+                    onToggle(isChecked)
+
+                    // Met à jour l'élément dans la liste des menuItems
+                    val itemIndex = menuItems.indexOfFirst { it.text == label }
+                    if (itemIndex != -1) {
+                        menuItems[itemIndex] = menuItems[itemIndex].copy(isChecked = isChecked)
+                    }
                 }
             )
         )
     }
 
-    override fun SubMenu(label: String, isEnabled: Boolean, submenuContent: TrayMenu.() -> Unit) {
+    override fun SubMenu(label: String, isEnabled: Boolean, submenuContent: TrayMenuBuilder.() -> Unit) {
         val subMenuItems = mutableListOf<WindowsTrayManager.MenuItem>()
-        val subMenuImpl = WindowsTrayMenuImpl(iconPath, tooltip).apply(submenuContent)
+        val subMenuImpl = WindowsTrayMenuBuilderImpl(iconPath, tooltip).apply(submenuContent)
         subMenuItems.addAll(subMenuImpl.menuItems)
 
         menuItems.add(
--- src/main/kotlin/com/kdroid/composetray/tray/impl/WindowsTrayInitializer.kt
+++ src/main/kotlin/com/kdroid/composetray/tray/impl/WindowsTrayInitializer.kt
@@ -1,14 +1,14 @@
 package com.kdroid.composetray.tray.impl
 
 import com.kdroid.composetray.lib.windows.WindowsTrayManager
-import com.kdroid.composetray.menu.TrayMenu
-import com.kdroid.composetray.menu.impl.WindowsTrayMenuImpl
+import com.kdroid.composetray.menu.api.TrayMenuBuilder
+import com.kdroid.composetray.menu.impl.WindowsTrayMenuBuilderImpl
 
 object WindowsTrayInitializer {
-    fun initialize(iconPath: String, tooltip: String, menuContent: TrayMenu.() -> Unit) {
+    fun initialize(iconPath: String, tooltip: String, menuContent: TrayMenuBuilder.() -> Unit) {
         val windowsTrayManager = WindowsTrayManager(iconPath, tooltip)
         // Create an instance of WindowsTrayMenuImpl and apply the menu content
-        val trayMenuImpl = WindowsTrayMenuImpl(iconPath).apply(menuContent)
+        val trayMenuImpl = WindowsTrayMenuBuilderImpl(iconPath).apply(menuContent)
         val menuItems = trayMenuImpl.build()
 
         // Add each menu item to WindowsTrayManager
