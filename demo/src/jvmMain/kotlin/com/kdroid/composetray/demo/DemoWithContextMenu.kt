effb95c3711eba16008d25dbc431efae6e82ee97
Elie G. <elyahou.hadass@gmail.com>
2025-04-28 14:21:48 +0300

    Simplify imports and optimize `mutableStateOf` usage.
    Unused imports were removed to streamline the code and improve readability. Additionally, replaced `mutableStateOf` with `remember { mutableStateOf(...) }` to ensure state preservation across recompositions.

--- demo/src/jvmMain/kotlin/com/kdroid/composetray/demo/DemoWithContextMenu.kt
+++ demo/src/jvmMain/kotlin/com/kdroid/composetray/demo/DemoWithContextMenu.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.kdroid.composetray.tray.api.Tray
