c01e1275653cef544108c353e229cc7f83bf565b
Elie Gambache <elyahou.hadass@gmail.com>
2025-04-28 13:15:51 +0300

    Update build config and restructure native source files
    Removed hardcoded group and version from Gradle file and updated dependencies, including downgrading `kmplog`. Added DMG to native target formats and standardized package versioning. Moved native C files to a dedicated `winlib` directory for better organization.

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -10,9 +10,6 @@ kotlin {
     jvmToolchain(17)
 }
 
-group = "com.kdroid.composetray.demo"
-version = "0.5.5"
-
 repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
@@ -25,16 +22,16 @@ dependencies {
     implementation(compose.foundation)
     implementation(compose.desktop.currentOs)
     implementation(compose.material3)
-    implementation("io.github.kdroidfilter:kmplog:0.6.2")
+    implementation("io.github.kdroidfilter:kmplog:0.3.0")
 }
 
 compose.desktop {
     application {
         mainClass = "com.kdroid.composetray.demo.DynamicTrayMenuKt"
         nativeDistributions {
-            targetFormats(TargetFormat.Msi, TargetFormat.Deb)
+            targetFormats(TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Dmg)
             packageName = "tray-demo"
-            packageVersion = version.toString()
+            packageVersion = "1.0.0"
         }
     }
 }
