02f9cea1993bfcbe3b0083422bf03039e9828347
Elie Gambache <elyahou.hadass@gmail.com>
2025-08-19 23:39:23 +0300

    Update package declaration in `TrayAppDemo.kt`

--- demo/src/jvmMain/kotlin/com/kdroid/composetray/demo/TrayAppDemo.kt
+++ demo/src/jvmMain/kotlin/com/kdroid/composetray/demo/TrayAppDemo.kt
@@ -1,4 +1,4 @@
-package io.github.kdroidfilter.seforimapp.features.screens.bookcontent.state
+package com.kdroid.composetray.demo
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
